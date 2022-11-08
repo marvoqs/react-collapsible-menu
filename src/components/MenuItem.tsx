88bd7b01899f77a8bbca46c11e65fbe37f193201
Marek Schwarz <marvoqs@gmail.com>
2022-11-08 15:57:25 +0100

    Replace materialUI with styled-components in "demo"

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const MenuItem = styled.div`
+  display: inline-block;
+  display: flex;
+  flex: 0 0 auto;
+  margin: 0.85rem;
+`;
+
+export default MenuItem;
--- src/demo.js
+++ src/demo.js
@@ -1,99 +1,20 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
-import DeleteIcon from "@material-ui/icons/Delete";
-import CloudUploadIcon from "@material-ui/icons/CloudUpload";
-import KeyboardVoiceIcon from "@material-ui/icons/KeyboardVoice";
-import Icon from "@material-ui/core/Icon";
-import ArchiveIcon from "@material-ui/icons/Archive";
-import StarIcon from "@material-ui/icons/Star";
-import SaveIcon from "@material-ui/icons/Save";
-import DownloadIcon from "@material-ui/icons/ArrowDownward";
+
+import MenuItem from "./components/MenuItem";
 import IntersectionObserverWrapper from "./intersection-observer-wrapper";
-const useStyles = makeStyles((theme) => ({
-  button: {
-    margin: theme.spacing(1),
-    display: "flex",
-    flex: "0 0 auto"
-  }
-}));
 
 export default function IconLabelButtons() {
-  const classes = useStyles();
   return (
     <IntersectionObserverWrapper>
-      <Button
-        color="primary"
-        data-targetid="talk"
-        className={classes.button}
-        startIcon={<KeyboardVoiceIcon />}
-      >
-        Talk
-      </Button>
-      <Button
-        color="primary"
-        data-targetid="save"
-        className={classes.button}
-        startIcon={<SaveIcon />}
-      >
-        Save
-      </Button>
-      <Button
-        color="primary"
-        data-targetid="upload"
-        className={classes.button}
-        startIcon={<CloudUploadIcon />}
-      >
-        Upload
-      </Button>
-      <Button
-        color="primary"
-        data-targetid="download"
-        className={classes.button}
-        startIcon={<DownloadIcon />}
-      >
-        Download
-      </Button>
-      <Button
-        color="primary"
-        data-targetid="send"
-        className={classes.button}
-        startIcon={<Icon>send</Icon>}
-      >
-        Send
-      </Button>
-      <Button
-        color="primary"
-        data-targetid="star"
-        className={classes.button}
-        startIcon={<StarIcon />}
-      >
-        Star
-      </Button>
-      <Button
-        color="primary"
-        data-targetid="mark-as-read"
-        className={classes.button}
-        startIcon={<SaveIcon />}
-      >
-        Mark as Read
-      </Button>
-      <Button
-        color="primary"
-        data-targetid="archive"
-        className={classes.button}
-        startIcon={<ArchiveIcon />}
-      >
-        Archive
-      </Button>
-      <Button
-        color="primary"
-        data-targetid="delete"
-        className={classes.button}
-        startIcon={<DeleteIcon />}
-      >
-        Delete
-      </Button>
+      <MenuItem data-targetid="talk">Talk</MenuItem>
+      <MenuItem data-targetid="save">Save</MenuItem>
+      <MenuItem data-targetid="upload">Upload</MenuItem>
+      <MenuItem data-targetid="download">Download</MenuItem>
+      <MenuItem data-targetid="send">Send</MenuItem>
+      <MenuItem data-targetid="star">Star</MenuItem>
+      <MenuItem data-targetid="mark-as-read">Mark as Read</MenuItem>
+      <MenuItem data-targetid="archive">Archive</MenuItem>
+      <MenuItem data-targetid="delete">Delete</MenuItem>
     </IntersectionObserverWrapper>
   );
 }
